94bfd3789b21827dd15f4b4f6b475d3053a92d38
חתוכה <brhvangui12@gmail.com>
2023-04-02 03:54:08 +0300

    כניסה מגוגל עובד לא נראה ששולח סמס

---
+++ app/src/main/java/com/example/GameIsOn/MainActivity.java
@@ -0,0 +1,172 @@
+package com.example.GameIsOn;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.hbb20.CountryCodePicker;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText mgetphonenumber;
+    android.widget.Button msendotp;
+    CountryCodePicker mcountrycodepicker;
+    String countrycode;
+    String phonenumber;
+    ImageView googleAuth;
+    GoogleSignInClient client;
+    FirebaseAuth firebaseAuth;
+    ProgressBar mprogressbarofmain;
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    String codesent;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mcountrycodepicker = findViewById(R.id.countrycodepicker);
+        msendotp = findViewById(R.id.sendotpbutton);
+        msendotp.setOnClickListener(this);
+        mgetphonenumber = findViewById(R.id.getphonenumber);
+        mprogressbarofmain = findViewById(R.id.progressbarofmain);
+
+        googleAuth = findViewById(R.id.googleAuthBtn);
+        googleAuth.setOnClickListener(this);
+        GoogleSignInOptions options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        client = GoogleSignIn.getClient(this, options);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        countrycode = mcountrycodepicker.getSelectedCountryCodeWithPlus();
+
+        mcountrycodepicker.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
+            @Override
+            public void onCountrySelected() {
+                countrycode = mcountrycodepicker.getSelectedCountryCodeWithPlus();
+            }
+        });
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+                Toast.makeText(getApplicationContext(), "OTP is Sent", Toast.LENGTH_SHORT).show();
+                mprogressbarofmain.setVisibility(View.INVISIBLE);
+                codesent = s;
+                Intent intent = new Intent(MainActivity.this, otpAuthentication.class);
+                intent.putExtra("otp", codesent);
+                startActivity(intent);
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            Intent intent = new Intent(MainActivity.this, chatActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == msendotp) {
+            String number;
+            number = mgetphonenumber.getText().toString();
+            if (number.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Please Enter Your number", Toast.LENGTH_SHORT).show();
+            } else if (number.length() < 9) {
+                Toast.makeText(getApplicationContext(), "Please Enter correct number", Toast.LENGTH_SHORT).show();
+            } else {
+                mprogressbarofmain.setVisibility(View.VISIBLE);
+                phonenumber = countrycode + number;
+                PhoneAuthOptions options = PhoneAuthOptions.newBuilder(firebaseAuth)
+                        .setPhoneNumber(phonenumber)
+                        .setTimeout(60L, TimeUnit.SECONDS)
+                        .setActivity(MainActivity.this)
+                        .setCallbacks(mCallbacks)
+                        .build();
+                PhoneAuthProvider.verifyPhoneNumber(options);
+            }
+        }
+        if (view == googleAuth) {
+            SignIn();
+            Toast.makeText(getApplicationContext(), "Google Sign In", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void SignIn() {
+        Intent intent = client.getSignInIntent();
+        startActivityForResult(intent, 100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+       // if (resultCode != RESULT_CANCELED) {
+            if (requestCode == 100) {
+                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                try {
+                    GoogleSignInAccount account = task.getResult(ApiException.class);
+
+                    AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+                    FirebaseAuth.getInstance().signInWithCredential(credential)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Intent intent = new Intent(MainActivity.this, chatActivity.class);
+                                        startActivity(intent);
+                                    } else {
+                                        Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+
+                } catch (ApiException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        //}
+    }
+}
